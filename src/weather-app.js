cb9dd000745828fa444b4aceca0ee8b891730843
k-ira-85 <irenkra85@gmail.com>
2022-07-02 22:41:02 +0200

    Update weather-app.js

--- src/weather-app.js
+++ src/weather-app.js
@@ -31,7 +31,7 @@ function formatDay(timestamp) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "fdb87381c54f7996300ef6d0a16df4b7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -63,7 +63,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "fdb87381c54f7996300ef6d0a16df4b7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
