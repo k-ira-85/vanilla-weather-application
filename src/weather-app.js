e70212caac28a3daa2720e4a86f5887a9c3b270c
k-ira-85 <irenkra85@gmail.com>
2022-07-08 23:52:26 +0200

    added styles

--- src/weather-app.js
+++ src/weather-app.js
@@ -35,7 +35,7 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row">`;
+  let forecastHTML = `<div class="row white-bgs">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
